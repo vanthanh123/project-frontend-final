8dc74807f50e4785e1a75c6ca93b82860e63d3c2
vanthanh <thanhssntc2@gmail.com>
2021-09-04 20:48:11 +0700

    first commit

---
+++ src/components/Seeds/seed.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import './seed.css';
+
+const seeds = (Props) => {
+    return(
+        <>
+            <h1>seed</h1>
+        </>
+    )
+}
+
+
+export default seeds;
---
+++ src/components/tools/tools.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import './tools.css';
+
+const tools = (Props) => {
+    return(
+        <>
+            <h1>seed</h1>
+        </>
+    )
+}
+
+
+export default tools;
