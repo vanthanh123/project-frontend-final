fb12fd84aa509ff1b6dd6db87aaacd3cea5aaac5
tranthuong021001 <tranthuong021001@gmail.com>
2021-09-13 11:36:31 +0700

    complete seeds component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <>
-      
+      <Seeds />
       <Tools/>
     </>
   );
--- src/components/Seeds/seed.js
+++ src/components/Seeds/seed.js
@@ -1,13 +1,69 @@
-import React  from "react";
-import './seed.css';
+import React from "react";
+import "./seed.css";
 
 const seeds = (Props) => {
-    return(
-        <>
-            <h1>seed</h1>
-        </>
-    )
-}
-
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      </link>
+      
+      <div class="seeds">
+        <div class="seed">
+          <header>
+            <p class="title">MELON</p>
+          </header>
+          <content>
+            <div class="image">
+              <img class="img-fluid" src="/images/melon.png" alt="" />
+            </div>
+          </content>
+          <footer class="footer">
+            <div class="price">$200</div>
+          </footer>
+        </div>
+        <div class="seed">
+          <header>
+            <p class="title">APPLE</p>
+          </header>
+          <content>
+            <div class="image">
+              <img class="img-fluid" src="/images/apple.png" alt="" />
+            </div>
+          </content>
+          <footer class="footer">
+            <div class="price">$350</div>
+          </footer>
+        </div>
+        <div class="seed">
+          <header>
+            <p class="title">GRAPE</p>
+          </header>
+          <content>
+            <div class="image">
+              <img class="img-fluid" src="../images/grape.png" alt="" />
+            </div>
+          </content>
+          <footer class="footer">
+            <div class="price">$500</div>
+          </footer>
+        </div>
+        <div class="seed">
+          <header>
+          <p class="title"></p>
+          </header>
+          <content>
+            <div class="image"></div>
+          </content>
+          <footer class="footer">
+            <div class="price"> </div>
+          </footer>
+        </div>
+        <div class="component-name">current seeds</div>
+      </div>
+    </>
+  );
+};
 
 export default seeds;
