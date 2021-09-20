ff0418baa23bda06dbd3ce1bdd5880679866a98e
doanngochuy-12 <doanngochuy12@gmail.com>
2021-09-20 17:03:21 +0700

    update tools

--- src/components/tools/tools.js
+++ src/components/tools/tools.js
@@ -6,65 +6,65 @@ const tools = (Props) => {
         <>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <div class="tools">
-                <div class="o o1">
+                <div class="tool">
                     <header>
-                        <div class="s s1">
+                        <div class="title">
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
                         </div>
                     </header>
                     <content>
-                        <div class="a a1"><img class="img-fluid" src="./images/van-khoa-nuoc-2-chieu-kitz-removebg-preview-removebg-preview.png" alt=""/></div>
+                        <div class="image"><img class="img-fluid" src="./images/van-khoa-nuoc-2-chieu-kitz-removebg-preview-removebg-preview.png" alt=""/></div>
 
                     </content>
-                    <footer>
-                        <div class="g g1"></div>
+                    <footer class="footer">
+                        <div class="price"></div>
                     </footer>
                 </div>
-                <div class="o o2">
+                <div class="tool">
                     <header>
-                        <div class="s s2"><i class="fa fa-star"></i>
+                        <div class="s s2 title"><i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
                         </div>
                     </header>
                     <content>
-                        <div class="a a2"><img class="img-fluid" src="/images/binh-tuoi-cay-nhua.png" alt=""/></div>
+                        <div class="image"><img class="img-fluid" src="/images/binh-tuoi-cay-nhua.png" alt=""/></div>
                     </content>
-                    <footer>
-                        <div class="g g2"></div>
+                    <footer class="footer">
+                        <div class="price"></div>
                     </footer>
                 </div>
-                <div class="o o3">
+                <div class="tool">
                     <header>
-                        <div class="s s3"><i class="fa fa-star sao"></i>
+                        <div class="title"><i class="fa fa-star sao"></i>
                             <i class="fa fa-star sao"></i>
                             <i class="fa fa-star sao"></i>
                         </div>
                     </header>
                     <content>
-                        <div class="a a3"><img class="img-fluid" src={'../images/phan-removebg-preview.png'} alt=""/></div>
+                        <div class="image"><img class="img-fluid" src={'../images/phan-removebg-preview.png'} alt=""/></div>
                     </content>
-                    <footer>
-                        <div class="g3">$300</div>
+                    <footer class="footer">
+                        <div class="g3 price">$300</div>
                     </footer>
                 </div>
-                <div class="o o4">
+                <div class="tool">
                     <header>
-                        <div class="s s4"><i class="fa fa-star"></i>
+                        <div class="s s4 title"><i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star sao"></i>
                         </div>
                     </header>
                     <content>
-                        <div class="a a4"><img class="img-fluid" src="images/zdv1623894537-removebg-preview.png" alt=""/></div>
+                        <div class="image"><img class="img-fluid" src="images/zdv1623894537-removebg-preview.png" alt=""/></div>
                     </content>
-                    <footer>
-                        <div class="g4">$900</div>
+                    <footer class="footer">
+                        <div class="price">$900</div>
                     </footer>
                 </div>
-                <div class="current">current upgrades</div>
+                <div class="component-name">current upgrades</div>
             </div>
 
         </>
